984e99a38a10ca1b6ad6629d31b3acfab11e4617
giantchen <giantchen@9a2690af-839c-97be-f211-1e3b3f06765b>
2010-03-30 15:42:23 +0000

    Add BSD-license.

--- evproto/common.h
+++ evproto/common.h
@@ -1,3 +1,12 @@
+// Copyright 2010, Shuo Chen.  All rights reserved.
+// http://code.google.com/p/evproto/
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the License file.
+
+// Author: Shuo Chen (chenshuo at chenshuo dot com)
+//
+
 #ifndef EVPROTO_COMMON_H
 #define EVPROTO_COMMON_H
 
--- evproto/httpserver.h
+++ evproto/httpserver.h
@@ -1,6 +1,8 @@
 // Copyright 2010, Shuo Chen.  All rights reserved.
 // http://code.google.com/p/evproto/
 //
+// Use of this source code is governed by a BSD-style license
+// that can be found in the License file.
 
 // Author: Shuo Chen (chenshuo at chenshuo dot com)
 //
